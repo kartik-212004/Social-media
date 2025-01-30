556e8643a358c7474c094cc232b2567a8a6886ae
Kartik <kartik200421@gmail.com>
2025-01-31 02:49:27 +0530

    added next-auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,11 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Github from "next-auth/providers/github";
-
+import z from "zod";
+const mySchema = z.object({
+  email: z.string().email("Invalid Email"),
+  password: z.string().min(6, "Weak Strength"),
+});
 const handler = NextAuth({
   providers: [
     Github({
@@ -27,10 +31,14 @@ const handler = NextAuth({
           placeholder: "Password",
         },
       },
-      async authorize(credentials) {
-        console.log(credentials);
+      async authorize(credentials: { email: string; password: string }) {
+        const parse = mySchema.safeParse(credentials);
+        const { email, password } = credentials;
       },
     }),
   ],
+  pages: {
+    signIn: "/signin",
+  },
 });
 export { handler as GET, handler as POST };
