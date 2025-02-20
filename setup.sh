6069f0df17e2aa15e4bbddfb27c76dea37a8aa02
Kartik <kartik200421@gmail.com>
2025-02-20 19:50:07 +0530

    added setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Setting up your Next.js project"
+
+# exit on error
+set -e
+
+echo "Installing dependencies"
+npm install
+
+echo "🔧 Generating Prisma client"
+npx prisma generate
+
+echo "instaling dependencies"
+npm install
+
+echo "✅ Setup complete! You can now run 'npm run dev' to start the server."
