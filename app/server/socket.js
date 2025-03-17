b49ccb57ccf7b2cb4fe7257e10bcc0a1d5d50cc8
Kartik <kartik200421@gmail.com>
2025-03-17 19:40:24 +0530

    ws

--- app/server/socket.js
+++ app/server/socket.js
@@ -1,21 +1,26 @@
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
+let clientCount = 0;
 
 wss.on('connection', function connection(ws) {
-  console.log('Client connected');
+  clientCount++;
+  console.log(`Client connected: ${clientCount}`);
 
   ws.on('message', function message(data) {
     console.log('Received:', data.toString());
 
     wss.clients.forEach(client => {
-      if (client.readyState === ws.OPEN) {
+      if (client.readyState === WebSocket.OPEN) {
         client.send(data.toString());
       }
     });
   });
 
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    clientCount--;
+    console.log(`Client disconnected: ${clientCount}`);
+  });
 });
 
 console.log('WebSocket server running on ws://localhost:8080');
