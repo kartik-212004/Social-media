c20bab21a5d2d47f306fc1e5575702dfc3594499
Kartik <kartik200421@gmail.com>
2025-03-23 08:46:22 +0530

    fixed the previous commit

--- components/middle-bar.tsx
+++ components/middle-bar.tsx
@@ -281,9 +281,11 @@ export default function Middlebar() {
 
           <div className="mt-2 flex items-center space-x-3">
             <Heart className="text-zinc-500 hover:text-red-500 cursor-pointer" />
-            <Link className="text-zinc-500" href={post.imageUrl || "$"}>
-              <ExternalLink />
-            </Link>
+            {post.imageUrl && (
+              <Link className="text-zinc-500" href={post.imageUrl}>
+                <ExternalLink />
+              </Link>
+            )}
             <span className="text-zinc-500">0</span>
             {!tabBar && (
               <span
