fb3d4783f9f0dd56a46ae6a667e2215486723c56
Kartik <kartik200421@gmail.com>
2025-03-24 02:19:18 +0530

    added metadata

--- app/Wrapper.tsx
+++ app/Wrapper.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Sidebar from "@/components/left-sidebar";
 import BottomNavbar from "@/components/navbar";
@@ -13,7 +15,25 @@ export default function LayoutWrapper({
     pathname === "/signin" ||
     pathname === "/signup" ||
     pathname === "/not-found";
-    
+
+  useEffect(() => {
+    const originalTitle = document.title;
+
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        document.title = "Please Cum back! 💦";
+      } else {
+        document.title = originalTitle;
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
+
   return (
     <div className="flex min-h-screen">
       {!isAuthPage && <Sidebar />}
